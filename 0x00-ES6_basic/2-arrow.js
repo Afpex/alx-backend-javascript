b8c685d54668b1a3fc56377d0f1d9d8e6d46a83e
Anthony <bondolo90@gmail.com>
2024-01-07 14:01:44 +0300

    Task 3 change

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,19 @@
-export default function getNeighborhoodsList() {
-	  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+//export default function getNeighborhoodsList() {
+//	  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+//
+//	  const self = this;
+//	  this.addNeighborhood = (newNeighborhood) => {
+//		      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+//		      return self.sanFranciscoNeighborhoods;
+//		    };
+//}
 
-	  const self = this;
-	  this.addNeighborhood = (newNeighborhood) => {
-		      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		      return self.sanFranciscoNeighborhoods;
-		    };
+//rewrite a function using arrow syntax
+export default function getNeighborhoodsList() {
+	this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+	this.addNeighborhood = (newNeighborhood) => {
+		this.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return this.sanFranciscoNeighborhoods;
+	};
 }
